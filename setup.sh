cfd280bd065f5d8cd91eec7e0237a1b4501e4a35
lalanza808 <lalanza808@users.noreply.github.com>
2016-11-10 00:37:25 -0800

    i want to deploy my new servers with these dotfiles

--- setup.sh
+++ setup.sh
@@ -4,41 +4,24 @@
 # overwrites the current (usually default) .bashrc, .bash_profile, and .bash/ if it exists.
 # Use this script at your own risk. 
 
-echo -e "[!] WARNING!\n"
-
-echo -e "[!] This script overwrites the following files:\n\n"
-
-echo -e "\t- ~/.bashrc"
-
-echo -e "\t- ~/.bash_profile"
-
-echo -e "\t- ~/.bash/\n\n\n"
-
-sleep 3
-
-echo -e "[!] Proceed?\n"
-
-read -n 1 -p "[y|n] " answer
-
-if [[ ${answer} == 'y' || ${answer} == 'Y' ]]
-then
-	echo -e "\n\n[.] Copying rc files over to ~/.bash/\n\n"
-	cp -r -f bash ~/.bash
-	sleep 3
-	#
-	echo -e "[.] Copying necessary configs\n\n"
-	cp -f ~/.bash/configs/.vimrc ~/.vimrc
-	cp -f ~/.bash/.bashrc ~/.bashrc
-	cp -f ~/.bash/configs/.screenrc ~/.screenrc
-	echo "source ~/.bashrc" > ~/.bash_profile
-	sleep 3
-	#
-	echo -e "[.] Sourcing new .bashrc"
-	source ~/.bashrc
-	sleep 3
-	#
-	echo -e "[+] Done!\n\nEnjoi\n\n"
-	sleep 3
-	clear
-fi
-
+echo -e "I hope you read the script before executing it. Watch closely."
+sleep .5
+##
+echo -e "\n\n[.] Copying rc files over to ~/.bash/\n\n"
+cp -r -f bash ~/.bash
+sleep 1
+##
+echo -e "[.] Copying necessary configs\n\n"
+cp -f ~/.bash/configs/.vimrc ~/.vimrc
+cp -f ~/.bash/.bashrc ~/.bashrc
+cp -f ~/.bash/configs/.screenrc ~/.screenrc
+echo "source ~/.bashrc" > ~/.bash_profile
+sleep 1
+##
+echo -e "[.] Sourcing new .bashrc"
+source ~/.bashrc
+sleep 1
+##
+echo -e "[+] Done!\n\nEnjoi\n\n"
+sleep 2
+clear
